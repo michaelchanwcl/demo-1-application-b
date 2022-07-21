68d610fee4a9abfae71090fba7c4e6c2fde5ecc5
Michael <michael.chan@wclsolution.com>
2022-07-21 09:15:15 +0800

    Commit 1

--- src/main/java/com/mic/App_B_Controller.java
+++ src/main/java/com/mic/App_B_Controller.java
@@ -1,5 +1,7 @@
 package com.mic;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,10 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class App_B_Controller {
 
+    private static final Logger log = LoggerFactory.getLogger(App_B_Controller.class);
+
     @PostMapping("/b")
     public App_B_ResponseDTO post(@RequestBody App_B_RequestDTO requestDTO) {
         App_B_ResponseDTO appBResponseDTO = new App_B_ResponseDTO();
         appBResponseDTO.setA("Test by Michael with input=[" + requestDTO.getA() + "]");
+        log.info("Completed App B and return with result=[" + appBResponseDTO.getA() + "]");
         return appBResponseDTO;
     }
 }
